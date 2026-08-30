61f98d931542e61318f4aff5f349188227c1f521
Cristiano Carvalho <cristiano.carvalho23@icloud.com>
2026-08-30 13:13:47 -0300

    chore: release 0.5.0
    Update Aludel package metadata and changelog for 0.5.0.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Aludel.MixProject do
   def project do
     [
       app: :aludel,
-      version: "0.4.2",
+      version: "0.5.0",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
